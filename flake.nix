f3238b651dc66ba862a4db9e771344d98d5e0049
Sebastiano Giordano <46520354+Krahos@users.noreply.github.com>
2024-08-10 18:26:48 +0200

    Repo setup

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Rust devshell for Shuftle";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            cargo-watch
+            cargo-udeps
+            rust-analyzer
+            rust-bin.nightly.latest.default
+          ];
+        };
+      }
+    );
+}
