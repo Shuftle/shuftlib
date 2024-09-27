b6845c70d04ff5d9ddecdcfdfc4a55a5d8145a1d
Sebastiano Giordano <46520354+Krahos@users.noreply.github.com>
2024-09-27 17:58:22 +0200

    chore: Fixing CI, adding .envrc, flake, clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! This crate contains all the necessary types, methods, functions and traits
 //! to work with cards, decks and card games.
-
+#![expect(incomplete_features)]
 #![feature(generic_const_exprs)]
 
 /// Contains the logic relative to the briscola engine.
